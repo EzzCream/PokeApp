f574ca4c11914ce350039d7f4c647e9ab8a23b13
Oscar <orodriguezc1603@alumno.ipn.mx>
2023-02-09 22:06:23 -0600

    update with call api
    Se hizo la primera llamada a la poke api y se renderizo su nombre y imagen del pokemon

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import axios from 'axios';
+import { getRandomInt } from './helpers/getNumerRandom.js';
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+async function callPoke() {
+	const { data } = await axios.get(
+		`https://pokeapi.co/api/v2/pokemon/${getRandomInt(1008)}`,
+	);
+	const { name, sprites } = data;
+	const imgPokemon = sprites.other.home.front_default;
+	document.querySelector('#app').innerHTML = `
+    <div>
+      <h1>${name}</h1>
+      <img src="${imgPokemon}" alt="pokemon">
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  `;
+}
 
-setupCounter(document.querySelector('#counter'))
+callPoke();
